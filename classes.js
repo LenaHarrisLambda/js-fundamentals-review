bbf5504f5864424e4edd9ea9e98eea61948c8fe4
Lena Harris <kayoriayane@Lenas-MacBook-Pro.local>
2020-08-20 19:46:03 -0500

    Completed review questions.

--- classes.js
+++ classes.js
@@ -1,45 +1,66 @@
-/* 
-We have a school to build here! In this project you will get used to thinking about classes in JavaScript and building them from a brand new data set.
-*/
+/* We have a school to build here! In this project you will get used to thinking about classes in JavaScript and building them from a brand new data set. */
 
-/* 
-Pokemon
+/* Pokemon
  - First we need a Pokemon class. This will be our base-class
  - Pokemon receives name, height, weight, level as props
  - Every Pokemon should also be initialized with a health attribute set to 50
  - Pokemon receives sleep as a method.
-    - This method will return the pokemon's health to 100
-- Pokemon receives levelUp as a method.
-    - This method should increase the pokemon's level by 1
+  - This method will return the pokemon's health to 100
+ - Pokemon receives levelUp as a method.
+  - This method should increase the pokemon's level by 1 */
 
-FireType
+ /* FireType
  - Now that we have a Pokemon as our base class, we'll build our FireType class.
  - FireType uses the same attributes that have been set up by Pokemon
  - FireType has the following unique props:
-     - type property set to 'fire'
-     - nickname property
-     - weaknesses property initialized to an array with the values 'water', 'ground', 'rock'
+  - type property set to 'fire'
+  - nickname property
+  - weaknesses property initialized to an array with the values 'water', 'ground', 'rock'
  - FireType has the following methods:
-     - fireAttack which should return the string `${name} just blew a giant fireball.'
-*/
+  - fireAttack which should return the string `${name} just blew a giant fireball.' */
 
+class Pokemon {
+	constructor(attributes) {
+		this.name = attributes.name;
+		this.height = attributes.height;
+		this.weight = attributes.weight;
+		this.level = attributes.level;
+		this.health = 50;
+	}
+	sleep() {
+		this.health = 100;
+		return `${this.name} went to sleep.`
+	}
+	levelUp () {
+		this.level += 1;
+		return `${this.name} leveled up to ${this.level}.`
+	}
+}
 
-
-
+class FireType extends Pokemon {
+	constructor(attributes) {
+		super(attributes);
+		this.type = 'Fire';
+		this.nickname = attributes.nickname;
+		this.weaknesses = ['Water', 'Ground', 'Rock']
+	}
+	fireAttack() {
+		return `${this.nickname} the ${this.name} just blew a giant fireball.`
+	}
+}
 
 /* Uncomment below to test your code */
+const charmander = new FireType({
+	name: 'Charmander',
+	height: '23in',
+	weight: '18lbs',
+	level: 7,
+	nickname: 'Moeru',
+});
 
-// const charmander = new FireType({
-//     name: "Charmander",
-//     height: "23in",
-//     weight: "18lbs",
-//     level: 7
-// })
-
-// console.log(charmander.fireAttack()) // Hello my name is Severus charmander, I am from Hogwarts
-// console.log(charmander.health) // 50
-// console.log(charmander.level) // 7
-// console.log(charmander.sleep()) 
-// console.log(charmander.health) // 100
-// console.log(charmander.weaknesses) // ['water', 'ground', 'rock']
-
+console.log(charmander.fireAttack());
+console.log(charmander.health); // 50
+console.log(charmander.level); // 7
+console.log(charmander.sleep());
+console.log(charmander.health); // 100
+console.log(charmander.weaknesses); // ['water', 'ground', 'rock']
--- prototypes.js
+++ prototypes.js
@@ -1,94 +1,90 @@
-/*
-  Object oriented design is commonly used in video games.  For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
-  In this file you will be creating three constructor functions: GameObject, Humanoid.  
-  At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
-  
-  Each constructor function has unique properties and methods that are defined in their block comments below:
-*/
+/* Object oriented design is commonly used in video games. For this part of the assignment you will be implementing several constructor functions with their correct inheritance hierarchy.
+In this file you will be creating three constructor functions: GameObject, Humanoid.
+At the bottom of this file are 3 objects that all end up inheriting from Humanoid.  Use the objects at the bottom of the page to test your constructor functions.
+Each constructor function has unique properties and methods that are defined in their block comments below: */
+
+/* === GameObject ===
+ * createdAt
+ * name
+ * dimensions (These represent the character's size in the video game)
+ * destroy() // prototype method that returns: `${this.name} was removed from the game.`
+ * healthPoints
+ * takeDamage() // prototype method -> returns the string '<object name> took damage.' */
+
+/* === Humanoid (Having an appearance or character resembling that of a human.) ===
+ * team
+ * weapons
+ * language
+ * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
+ * should inherit destroy() from GameObject
+ * should inherit takeDamage() from GameObject */
 
 /*
-  === GameObject ===
-  * createdAt
-  * name
-  * dimensions (These represent the character's size in the video game)
-  * destroy() // prototype method that returns: `${this.name} was removed from the game.`
-  * healthPoints
-  * takeDamage() // prototype method -> returns the string '<object name> took damage.'
+	* Inheritance chain: GameObject -> Humanoid
+	* Instances of Humanoid should have all of the same properties as GameObject.
 */
 
-/*
-  === Humanoid (Having an appearance or character resembling that of a human.) ===
-  * team
-  * weapons
-  * language
-  * greet() // prototype method -> returns the string '<object name> offers a greeting in <object language>.'
-  * should inherit destroy() from GameObject 
-  * should inherit takeDamage() from GameObject
-*/
- 
-/*
-  * Inheritance chain: GameObject -> Humanoid
-  * Instances of Humanoid should have all of the same properties as GameObject.
-*/
+// GameObject //
+function GameObject(attributes) {
+  this.createdAt = attributes.createdAt;
+  this.name = attributes.name;
+	this.dimensions = attributes.dimensions;
+	this.healthPoints = attributes.healthPoints;
+}
+GameObject.prototype.destroy = function() {
+	return `${this.name} was removed from the game.`
+}
+GameObject.prototype.takeDamage = function() {
+	return `${this.name} took damage.`;
+}
 
-// Test you work by un-commenting these 3 objects and the list of console logs below:
+// Humanoid //
+function Humanoid(attributes) {
+	GameObject.call(this, attributes);
+	this.team = attributes.team;
+	this.weapons = attributes.weapons;
+	this.language = attributes.language;
+}
+Humanoid.prototype = Object.create(GameObject.prototype);
+Humanoid.prototype.greet = function() {
+	return `${this.name} offers a greeting in ${this.language}.`
+}
 
-/*
-  const mage = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 1,
-      height: 1,
-    },
-    healthPoints: 5,
-    name: 'Bruce',
-    team: 'Mage Guild',
-    weapons: [
-      'Staff of Shamalama',
-    ],
-    language: 'Common Tongue',
-  });
-  const swordsman = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 2,
-      width: 2,
-      height: 2,
-    },
-    healthPoints: 15,
-    name: 'Sir Mustachio',
-    team: 'The Round Table',
-    weapons: [
-      'Giant Sword',
-      'Shield',
-    ],
-    language: 'Common Tongue',
-  });
-  const archer = new Humanoid({
-    createdAt: new Date(),
-    dimensions: {
-      length: 1,
-      width: 2,
-      height: 4,
-    },
-    healthPoints: 10,
-    name: 'Lilith',
-    team: 'Forest Kingdom',
-    weapons: [
-      'Bow',
-      'Dagger',
-    ],
-    language: 'Elvish',
-  });
-  console.log(mage.createdAt); // Today's date
-  console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
-  console.log(swordsman.healthPoints); // 15
-  console.log(mage.name); // Bruce
-  console.log(swordsman.team); // The Round Table
-  console.log(mage.weapons); // Staff of Shamalama
-  console.log(archer.language); // Elvish
-  console.log(archer.greet()); // Lilith offers a greeting in Elvish.
-  console.log(mage.takeDamage()); // Bruce took damage.
-  console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
-*/
+// Test your work by un-commenting these 3 objects and the list of console logs below:
+const mage = new Humanoid({
+	createdAt: new Date(),
+	dimensions: {length: 2, width: 1, height: 1,},
+	healthPoints: 5,
+	name: 'Bruce',
+	team: 'Mage Guild',
+	weapons: ['Staff of Shamalama',],
+	language: 'Common Tongue',
+});
+const swordsman = new Humanoid({
+	createdAt: new Date(),
+	dimensions: {length: 2, width: 2, height: 2,},
+	healthPoints: 15,
+	name: 'Sir Mustachio',
+	team: 'The Round Table',
+	weapons: ['Giant Sword', 'Shield',],
+	language: 'Common Tongue',
+});
+const archer = new Humanoid({
+	createdAt: new Date(),
+	dimensions: {length: 1,width: 2,height: 4,},
+	healthPoints: 10,
+	name: 'Lilith',
+	team: 'Forest Kingdom',
+	weapons: ['Bow','Dagger',],
+	language: 'Elvish',
+});
+console.log(mage.createdAt); // Today's date
+console.log(archer.dimensions); // { length: 1, width: 2, height: 4 }
+console.log(swordsman.healthPoints); // 15
+console.log(mage.name); // Bruce
+console.log(swordsman.team); // The Round Table
+console.log(mage.weapons); // Staff of Shamalama
+console.log(archer.language); // Elvish
+console.log(archer.greet()); // Lilith offers a greeting in Elvish.
+console.log(mage.takeDamage()); // Bruce took damage.
+console.log(swordsman.destroy()); // Sir Mustachio was removed from the game.
